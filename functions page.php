8d01146c707d998b49d3c87a0d29eb09571151b9
Alec <alecshelembe@gmail.com>
2021-12-14 11:20:34 +0200

    Update functions page.php

--- functions page.php
+++ functions page.php
@@ -1,9 +1,10 @@
 <?php
 // sever
-$dbsevername = "hidden";
-$dbusername = "hidden";
-$dbpassword = "hidden";
-$dbname = "hidden";
+$dbsevername = "localhost";
+$dbusername = "";
+$dbpassword = "";
+$dbname = "";
+
 $conn = mysqli_connect($dbsevername, $dbusername, $dbpassword);
 
 mysqli_select_db($conn, $dbname);
@@ -13,28 +14,20 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\SMTP;
 
-require_once "PHPMailer/src/PHPMailer.php";
-require_once "PHPMailer/src/SMTP.php";
-require_once "PHPMailer/src/Exception.php";
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/PHPMailer/src/PHPMailer.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/PHPMailer/src/SMTP.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/PHPMailer/src/Exception.php";
 
 function stop(){
 	die();
 }
 
-function sanitizeString($var) {    
-	if (get_magic_quotes_gpc())
-
-		$var = stripsloashes($var);   
-	$var = htmlentities($var);    
-	$var = strip_tags($var); 
-
-	if (strlen($var) > 400 ) {
-		stop("Charachter break fatal error"); 
-	}
-	$var = addslashes($var);
-	return $var; 
+function alert($var) {
+	echo("<script type=\"text/javascript\">
+		alert(\"$var\");
+		</script>");
 }
-
 
 function go_to($var){
 
@@ -65,18 +58,27 @@ function check_if_empty($var) {
 	}
 }
 
+function sanitizeString($var) {    
+	if (get_magic_quotes_gpc())
 
-function alert($var) {
-	echo("<script type=\"text/javascript\">
-		alert(\"$var\");
-		</script>");
+	$var = stripsloashes($var);   
+	$var = htmlentities($var);    
+	$var = strip_tags($var); 
+
+	if (strlen($var) > 10000 ) {
+		alert("Charachter break fatal error"); 
+		redirect_back();
+	}
+	$var = addslashes($var);
+	return $var; 
+}
+function get_number($var){
+	// $number = preg_replace('/\D+/', '', "$var");
+	return $number;
 }
 
 function logout() {
 	session_destroy();
-	$location = "index.php";
-	alert("You have logged out");
-	go_to($location);
 }
 
 function post_check($var){
@@ -84,7 +86,7 @@ function post_check($var){
 		alert("$var input left blank.");
 		redirect_back();
 	}
-	$var = sanitizeString($_POST[$var]);
+	$var = $_POST[$var];
 	check_if_empty($var);
 	return $var;
 }
@@ -105,35 +107,55 @@ function confirm_match($var,$var2) {
 	} else {
 		alert("Password do not match");
 		redirect_back();
-		stop();
 	}
 }
 
 function check_login_email(){
 
-	$email = $_SESSION['email'];
+	if (isset($_SESSION['email'])) {
+		$email = $_SESSION['email'];
+		$name = $_SESSION['name'];
+		$number = $_SESSION['number'];
+		$account_status = $_SESSION['account_status'];
+		# code...
+	} else {
+		$email =  'none';
+		$name = 'none';
+		$number = 'none';
+		$account_status = 'none';
 
-	if (!isset($email)) {
-		$location = "index.php";
-		alert("Please login to continue");
-		go_to($location);
 	}
 
-	return $email;
+	$session_details = array($email, $name, $number);
+	return ($session_details);	
 }
 
-function check_login_name(){
 
-	$name = $_SESSION['name'];
+function get_device() {
+    return preg_match("/(android|avantgo|blackberry|bolt|boost|cricket|docomo|fone|hiptop|mini|mobi|palm|phone|pie|tablet|up\.browser|up\.link|webos|wos)/i", $_SERVER["HTTP_USER_AGENT"]);
 
-	if (!isset($name)) {
-		$location = "index.php";
-		alert("Please login to continue");
-		go_to($location);
+}
+
+function get_ipnumber() {
+	
+	//whether ip is from share internet
+	if (!empty($_SERVER['HTTP_CLIENT_IP']))   
+	{
+		$ip_address = $_SERVER['HTTP_CLIENT_IP'];
 	}
-
-	return $name;
+//whether ip is from proxy
+	elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR']))  
+	{
+		$ip_address = $_SERVER['HTTP_X_FORWARDED_FOR'];
+	}
+//whether ip is from remote address
+	else
+	{
+		$ip_address = $_SERVER['REMOTE_ADDR'];
+	}
+	return $ip_address;
 }
+
 
 function check_if_exists($varconn,$dbname,$table,$row_title,$info){
 
@@ -154,24 +176,6 @@ function check_if_exists($varconn,$dbname,$table,$row_title,$info){
 	}
 }
 
-function get_result($varconn,$dbname,$table,$row_title,$info){
-
-	$query = "SELECT `$row_title` FROM `$table` WHERE `$row_title` = '$info';";
-
-	// echo"$query"; 
-	// stop("$query");
-
-	$result = mysqli_query($varconn, $query) or die(mysqli_error($varconn));
-
-	$row = mysqli_num_rows($result);
-	if ($row > 0) {
-
-		return 'yes';
-	}	else {
-		return 'no';
-	}
-}
-
 function insert_info($varconn,$dbname,$table,$row_title,$info){
 	
 	$query = "INSERT INTO `$table` (`$row_title`) VALUES ('$info');";
@@ -183,9 +187,9 @@ function insert_info($varconn,$dbname,$table,$row_title,$info){
 }
 
 
-function update_info($varconn,$dbname,$table,$row_title,$row_title_2,$info,$email){
+function update_info($varconn,$dbname,$table,$row_title,$identifier_row,$info,$identifier_info){
 	
-	$query = "UPDATE `$table` SET `$row_title` = '$info' WHERE `$table`.`$row_title_2` = '$email';";
+	$query = "UPDATE `$table` SET `$row_title` = '$info' WHERE `$table`.`$identifier_row` = '$identifier_info';";
 
 	// echo"$query"; 
 	// stop("$query");
@@ -195,9 +199,9 @@ function update_info($varconn,$dbname,$table,$row_title,$row_title_2,$info,$emai
 	// stop("$query");
 }
 
-function pair_for_login($varconn,$table,$email,$email_info,$security_key,$security_key_info) {
+function pair_for_login($varconn,$table,$row_title,$info,$security_key,$security_key_info) {
 
-	$query = "SELECT * FROM $table WHERE $email = '$email_info';";
+	$query = "SELECT * FROM `$table` WHERE `$row_title` = '$info';";
 	
 	$result = mysqli_query($varconn, $query) or die(mysqli_error($varconn));
 	
@@ -208,27 +212,25 @@ function pair_for_login($varconn,$table,$email,$email_info,$security_key,$securi
 	if ($row == 0) {
 		alert("No account is registerd to this email");
 		redirect_back();
-		stop();
 	}
 
-	$status = return_info($varconn,'users','verificated','email',$email_info);
-
-	if ($status == 'no') {
-		alert('To continue, please verify your account via email.');
-		redirect_back();
+	$email_verified = return_info($varconn,'users','email_verified',$row_title,$info);
+	if ($email_verified == null) {
+		alert('Please verify your email.');
+		$location = "https://kingproteas.com/verification-page.php";
+		go_to($location);
 	}
 
-	$active = "";
+	$account_status = "";
 	$security_key = "";
 	
 	while ($row = mysqli_fetch_assoc($result)) {
-		$active = $row['active'];
-		$security_key = $row['security_key'];
+		$account_status = $row['account_status'];
+		$security_key = $row['password'];
 	}
 	
-
-	if ($active == "no" ){
-		alert("Not allowed");
+	if ($account_status == 'deactivaed') {
+		alert('Account not active');
 		redirect_back();
 	}
 
@@ -242,95 +244,43 @@ function pair_for_login($varconn,$table,$email,$email_info,$security_key,$securi
 		stop();
 	}
 
-	$query = "SELECT * FROM $table WHERE $email = '$email_info';";
+	$query = "SELECT * FROM `$table` WHERE `$row_title` = '$info';";
 
 	$result = mysqli_query($varconn, $query) or die(mysqli_error($varconn));
 
-
-	$active = "";
-	
-	while ($row = mysqli_fetch_assoc($result)) {
-		$active = $row['active'];
-	}
-	
-
-	if ($active == "no" ){
-		alert('Not allowed');
-		$location = "index.php";
-		go_to($location);
-	}
-
-	$query = "SELECT * FROM $table WHERE $email = '$email_info';";
-
-	$result = mysqli_query($varconn, $query) or die(mysqli_error($varconn));
 
 	while ($row = mysqli_fetch_assoc($result))
 	{ 
-
 		$email = $_SESSION['email'] = $row['email'];
 		$name = $_SESSION['name'] = $row['name'];
+		$number = $_SESSION['number'] = $row['number'];
+		$account_status = $_SESSION['account_status'] = $row['account_status'];
 	}
+
+	$date = "Date " . date("Y-m-d @ h:i:s a");
+
+	update_info($varconn,'','users','last_login','email',$date,$email);
 
 	alert("You have been logged in");
 
-	$location = "home page.php";
 	// https://github.com/alecshelembe
-	go_to($location);
 
 }
 
-function return_info($varconn,$table,$row_title,$row_title_2,$email){
+function return_info($varconn,$table,$row_title,$identifier_row,$identifier_info){
 	
-	$query = "SELECT $row_title FROM $table WHERE $row_title_2 = '$email' ;";
+	$query = "SELECT `$row_title` FROM `$table` WHERE `$table`.`$identifier_row` = '$identifier_info';";
 	
 	$result = mysqli_query($varconn, $query);
 	
-	
+	// echo("$query");
+	// stop();
 	while ($row = mysqli_fetch_assoc($result)) {
 		$value = $row["$row_title"];
+
 	}
+
 	return $value;
-}
-
-function show_reviews($varconn,$dbname,$table){
-
-	$query = "SELECT * FROM `$table`;";
-	
-// 	echo"$query"; 
-
-	$result = mysqli_query($varconn, $query);
-	
-	while ($row = mysqli_fetch_assoc($result)) {
-		$rating = $row['rating'];
-		$name = $row['name'];
-		$comment = $row['comment'];
-
-		echo"$name $comment $rating <br>";
-
-	}
-}
-
-function show_shops($varconn,$dbname,$table,$value){
-
-	$query = "SELECT * FROM `$table` WHERE `active` = '$value';";
-	
-	// echo"$query"; 
-	// stop();
-
-	$result = mysqli_query($varconn, $query);
-	
-	while ($row = mysqli_fetch_assoc($result)) {
-		$name = $row['name'];
-		$city = $row['city'];
-		$province = $row['province'];
-		
-		echo "<form action=\"site page.php\" method=\"get\">
-		<input type=\"submit\" name=\"site\" value =\"$name\">
-		</form>";
-
-	}
-
-
 }
 
 function remove($varconn,$dbname,$table,$row_title,$info){
@@ -339,131 +289,81 @@ function remove($varconn,$dbname,$table,$row_title,$info){
 
 	$result = mysqli_query($varconn, $query) or die(mysqli_error($varconn));
 
-	echo("$query");
-	stop();
-
 }
 
-function show_product($varconn,$dbname,$table,$value,$shop){
+function show_products($varconn,$dbname,$table,$status){
 
-	$query = "SELECT * FROM `$table` WHERE `active` = '$value' and `shop` = '$shop' and `state` = 'current';";
-
-	
-	// echo"$query"; 
-	// stop();
-
-	$result = mysqli_query($varconn, $query);
-	
-	$row = mysqli_num_rows($result);
-	if ($row == 0) {
-		echo "";
-	} else {
-
-		while ($row = mysqli_fetch_assoc($result)) {
-			$shop = $row['shop'];
-			$name = $row['name'];
-			$description = $row['description'];
-			$image = $row['image'];
-			$price = $row['price'];
-			$id = $row['id'];
-
-			$product = array("$shop", "$name", "$description", "$image", "$price");
-
-			$shop = $product[0];
-			$name = $product[1];
-			$description = $product[2];
-			$image = $product[3];
-			$price = $product[4];
-
-			echo"<h3></h3>
-			<div class='product_circle'>
-			<img src='product images/$image' class='product_picture'>
-			<p class='product_tag'>
-			<span class='product_tag_description'>Name: </span>$name<br>
-			<span class='product_tag_description'>Price: </span>$price<br>
-			<span class='product_tag_description'>Description: </span>$description<br>
-			<span class='product_tag_description small_font'>Id: $id</span>
-			</p>
-			<input type=\"button\" name=\"\" onClick='add(\"$id\");' value=\"Add to cart\">
-			<input type=\"button\" name=\"\" onClick='remove(\"$id\");' value=\"Remove\">
-			</div>";
-
-			return $product; 
-
-		}
-
-	}
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_products.php");
 }
 
-function show_product_on_menu($varconn,$dbname,$table,$value,$shop){
+function show_product_in_cart($varconn,$dbname,$table,$status,$id){
 
-	$query = "SELECT * FROM `$table` WHERE `active` = '$value' and `shop` = '$shop';";
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_product_in_cart.php");
+}
 
-	
-	// echo"$query"; 
-	// stop();
+function show_products_to_change($varconn,$dbname,$table,$status){
 
-	$result = mysqli_query($varconn, $query);
-	
-	$row = mysqli_num_rows($result);
-	if ($row == 0) {
-		echo "";
-	} else {
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_products_to_change.php");
+}
 
-		while ($row = mysqli_fetch_assoc($result)) {
-			$shop = $row['shop'];
-			$name = $row['name'];
-			$description = $row['description'];
-			$image = $row['image'];
-			$price = $row['price'];
-			$id = $row['id'];
+function show_products_in_catagory($varconn,$dbname,$table,$status,$catagory){
 
-			$product = array("$shop", "$name", "$description", "$image", "$price");
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_products_in_catagory.php");
+}
 
-			$shop = $product[0];
-			$name = $product[1];
-			$description = $product[2];
-			$image = $product[3];
-			$price = $product[4];
+function show_one_product($varconn,$dbname,$table,$status,$id){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_one_product.php");
+}
 
-			echo"<h3></h3>
-			<div class='product_circle_on_menu'>
-			<img src='product images/$image' class='product_picture'>
-			<p class='product_tag'>
-			<span class='product_tag_description'>Name: </span>$name<br>
-			<span class='product_tag_description'>Price: </span>$price<br>
-			<span class='product_tag_description'>Description: </span>$description<br>
-			<span class='product_tag_description small_font'>Id: $id</span>
-			</p>
-			</div>";
+function users_action($varconn,$dbname,$table){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_users.php");
+}
 
-			return $product; 
+function show_one_user($varconn,$dbname,$table,$row_title,$info){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_one_user.php");
+}
 
-		}
+function show_user_orders($varconn,$dbname,$table,$row_title,$info){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_user_orders.php");
+}
 
-	}
+function show_payments_successful($varconn,$dbname,$table,$row_title,$info){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_payments_successful.php");
+}
+
+function show_payments_pending($varconn,$dbname,$table,$row_title,$info){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_payments_pending.php");
+}
+
+function orders_action($varconn,$dbname,$table){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_orders.php");
+}
+
+function show_invoices($varconn,$dbname,$table,$row_title,$info){
+	include($_SERVER['DOCUMENT_ROOT'] . "/loops/show_invoices.php");
 }
 
 function image_process($varconn,$dir,$image,$file_type,$file_size,$file_tem_loc){
 
 
-	if( is_dir($dir) === false )
-	{
-		mkdir($dir);
-	}
+	if (is_dir($dir) === false){
+			mkdir($dir);
+		}
 	switch($file_type)
 	{
 		case 'image/jpeg':  $ext = 'jpg';   break;
 		case 'image/gif':   $ext = 'gif';   break;
 		case 'image/png':   $ext = 'png';   break;
 		case 'image/tiff':  $ext = 'tiff';  break;	
+		case 'image/jfif':  $ext = 'jfif';  break;	
 		default:       
-		alert("$image is not a valid image file $file_type unallowed");
+		alert("$file_type is not a valid image file $image unallowed");
 		redirect_back();
 	} 
 
 	if ($ext)
 	{	
+		$image = "$image".'.'."$ext";
 
 		$file_store = "$dir/$image";
 
@@ -491,22 +391,25 @@ function send_email($sender_email,$reciever_email,$reciever_name,$subject,$body)
     //Server settings
     // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
-    $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+    $mail->Host       = '';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = "$sender_email";                     //SMTP username
-    $mail->Password   = 'hidden';                               //SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 'hidden';                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+    $mail->Password   = '';                               //SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+    $mail->Port       = ;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+
+    //TCP port to connect to, use 587 for `PHPMailer::ENCRYPTION_STARTTLS` above
 
     //Recipients
     $mail->setFrom("$sender_email", 'Noreply');
     $mail->addAddress("$reciever_email");     //Add a recipient
-    $mail->addAddress("$sender_email");               //Name is optional
+    // $mail->addAddress("$sender_email");               //Name is optional
     //$mail->addReplyTo('info@example.com', 'Information');
-    //$mail->addCC('cc@example.com');
+    $mail->addCC('');
     //$mail->addBCC('bcc@example.com');
 
     //Attachments
+    //Add attachments
     //$mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
     //$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
 
@@ -517,10 +420,11 @@ function send_email($sender_email,$reciever_email,$reciever_name,$subject,$body)
     $mail->AltBody = "$body";
 
     $mail->send();
-    alert("We sent you an email, please see your inbox");
-} catch (Exception $e) {
+    alert("We sent you an email, please see your inbox or spam folder");
+	} catch (Exception $e) {
 	alert("Oops. Something went wrong. You might not recieve an email");
-}
+	// stop();
+	}
 
 	///////////////////////////////////////////////////////////////
 }
